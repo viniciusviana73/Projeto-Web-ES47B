e7e224436fd340c5bdf2ece0457c54bd9eb2c45a
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2024-11-17 17:29:05 -0300

    spinner onload

--- ES47B-P1/src/components/Main.jsx
+++ ES47B-P1/src/components/Main.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WeatherForm from './WeatherForm';
 import WeatherData from './WeatherData';
+import Spinner from './Spinner';
 
 const Main = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => { setIsLoading(false) }, 500);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="flex flex-col lg:flex-row items-center justify-center">
-            <div className="w-full lg:w-1/2 p-4">
+            {isLoading && (
+                <div className="w-full lg:w-1/2 p-4 flex justify-center items-center">
+                    <Spinner />
+                </div>
+            )}
+
+            <div className={`w-full lg:w-1/2 p-4 transition-opacity duration-500 ${isLoading ? 'hidden opacity-0' : 'opacity-100'}`}>
                 <h2 className="max-w-md mx-auto mb-3 text-white text-xl">Pesquisar clima por cidade</h2>
                 <WeatherForm />
             </div>
-            <div className="w-full lg:w-1/2 p-4">
+
+            <div className={`w-full lg:w-1/2 p-4 transition-opacity duration-500 ${isLoading ? 'hidden opacity-0' : 'opacity-100'}`}>
                 <WeatherData />
             </div>
         </div>
