17a13fb16c0454ab9bf35c1ec1d0c7f6ad953501
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2024-11-17 16:46:43 -0300

    App.jsx using UserLocationProvider

--- ES47B-P1/src/App.jsx
+++ ES47B-P1/src/App.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { UserLocationProvider } from './contexts/UserLocationContext';
 
-// TODO - Enviar request API IP para montar interface inicial usando lazy
 const App = () => {
-  return (
-    <>
-      <div className="min-h-screen flex flex-col bg-neutral-900">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    </>
-  );
+    return (
+        <UserLocationProvider>
+            <div className="min-h-screen flex flex-col bg-neutral-900">
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+        </UserLocationProvider>
+    );
 };
 
 export default App;
