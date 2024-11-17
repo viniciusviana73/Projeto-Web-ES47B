e1901fd43443947de456973a51bfa4bdaeb839ec
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2024-11-17 19:35:29 -0300

    Update Alert.jsx
    timeout to selfdestroy

--- ES47B-P1/src/components/Alert.jsx
+++ ES47B-P1/src/components/Alert.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// TODO - Caso timeout, componente é auto destruído após {timeout}ms
 const Alert = ({ message, title, type, timeout, width }) => {
+    const [isVisible, setIsVisible] = useState(true);
+
     const typeClasses = type === 'success'
         ? 'bg-green-100 border-green-400 text-green-700'
         : 'bg-orange-100 border-orange-500 text-orange-700';
+
+    useEffect(() => {
+        if (timeout) {
+            const timer = setTimeout(() => { setIsVisible(false) }, timeout);
+            return () => clearTimeout(timer);
+        }
+    }, [timeout]);
+
+    if (!isVisible) return null;
 
     return (
         <div className={`p-4 mt-4 ${width} mx-auto border-l-4 ${typeClasses}`} role="alert">
@@ -12,6 +22,6 @@ const Alert = ({ message, title, type, timeout, width }) => {
             <p>{message}</p>
         </div>
     );
-}
+};
 
 export default Alert;
