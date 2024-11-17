923d6c31da1ea19c01664a0e997d9add949b8315
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2024-11-17 16:51:58 -0300

    weatherForm component
    - fetch weatherstack api
    - input validation
    - createPortal - output validation

--- ES47B-P1/src/components/WeatherForm.jsx
+++ ES47B-P1/src/components/WeatherForm.jsx
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import React, { useState, useContext } from 'react';
+import Alert from './Alert';
+import { UserLocationContext } from '../contexts/UserLocationContext';
 
 const WeatherForm = () => {
-    const [city, setCity] = useState('');
-    const [unit, setUnit] = useState('m');
+    const { userLocation, setSearchParams, setWeatherData } = useContext(UserLocationContext);
+    const [city, setCity] = useState(userLocation.searchCity || userLocation.city || '');
+    const [unit, setUnit] = useState(userLocation.searchUnit || 'm');
+    const [inputError, setInputError] = useState(null);
+    const [apiError, setApiError] = useState(null);
+
+    const handleForm = async (e) => {
+        e.preventDefault();
+
+        if (!city.trim()) {
+            return setInputError('Por favor, insira o nome de uma cidade.');
+        }
+
+        setInputError(null);
+        setSearchParams({ city, unit });
+
+        // Dispara requisição para a API
+        try {
+            const key = 'x'// '4efddb1874bb89ae15eff63c678e61e3';
+            const response = await fetch(`http://api.weatherstack.com/current?access_key=${key}&query=${city}&units=${unit}`);
+            const data = await response.json();
+
+            if (data.error) {
+                throw new Error(data.error.info || "Erro ao buscar dados do clima.");
+            }
+
+            setWeatherData(data);
+        } catch (error) {
+            console.error('Erro ao buscar dados do clima:', error);
+            setApiError(`${error}`);
+        }
+    };
 
     return (
         <>
-            <form className="max-w-md mx-auto">
+            <form className="max-w-md mx-auto" onSubmit={handleForm}>
                 <div className="mb-4">
                     <label htmlFor="city" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
-                        Cidade {city} {unit}
+                        Cidade
                     </label>
                     <input type="text" id="city" onChange={(ev) => { setCity(ev.target.value) }} className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-stone-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Digite a cidade" value={city} />
                 </div>
@@ -31,6 +64,27 @@ const WeatherForm = () => {
                         Buscar
                     </button>
                 </div>
+
+                {inputError && (
+                    <Alert
+                        message={inputError}
+                        type="error"
+                        title="Entrada inválida"
+                        timeout={2000}
+                    />
+                )}
+
+                {apiError && (
+                    createPortal(
+                        <Alert
+                            message={apiError}
+                            type="error"
+                            title="Erro de Comunicação com API weatherstack:"
+                            width="w-full max-w-sm"
+                        />,
+                        document.getElementById('weather-alert')
+                    )
+                )}
             </form>
         </>
     );
