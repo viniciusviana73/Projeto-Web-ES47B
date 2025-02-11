35ac3b1b15ae18cff93c4d43eba8b2f1c8a00184
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2025-02-11 18:26:57 -0300

    app.js
    morgan logger
    db connection

---
+++ ES47B-P2-Backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// App init
+const app = express();
+const PORT = process.env.PORT || 1414;
+global.IS_PRODUCTION = process.env.PRODUCTION === "true";
+
+// DB connection
+const connect = require('./src/database/connection');
+connect();
+
+// App setup
+app.use(cookieParser());
+app.use(morgan('combined'));
+app.use(express.json());
+
+// Importing router.js
+const router = require('./src/router/router');
+app.use(router);
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
