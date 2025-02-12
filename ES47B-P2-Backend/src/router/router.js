bc9332dc9894972c185615bdef976e1358f8c47e
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2025-02-12 16:14:06 -0300

    Update router.js
    cache de respostas servidor

--- ES47B-P2-Backend/src/router/router.js
+++ ES47B-P2-Backend/src/router/router.js
@@ -2,20 +2,23 @@ const router = require("express").Router();
 const UserController = require("../controllers/UserController");
 const { jwtAuth } = require("../middlewares/jwtAuth");
 const CityWeatherController = require("../controllers/CityWeatherController");
+const { invalidateCache, cache } = require("../middlewares/redisCache");
 
 // Auth API
 router.get("/api/auth", jwtAuth, UserController.checkAuth);
 router.post("/api/auth", UserController.login);
 
 // User 
-router.get("/api/user", jwtAuth, UserController.getUser);
-router.post("/api/user", UserController.createUser);
+router.get("/api/user", jwtAuth, cache.route(), UserController.getUser);
+router.post("/api/user", invalidateCache('/api/user'), UserController.createUser);
 
 // CityWeather API
-router.get("/api/cityweather", jwtAuth, CityWeatherController.getCityWeather);
-router.post("/api/cityweather", jwtAuth, CityWeatherController.createCityWeather);
-router.put("/api/cityweather", jwtAuth, CityWeatherController.editCityWeather);
-router.delete("/api/cityweather", jwtAuth, CityWeatherController.deleteCityWeather);
+router.get("/api/city", jwtAuth, cache.route(), CityWeatherController.getCityWeather);
+router.post("/api/city", jwtAuth, invalidateCache('/api/city'), CityWeatherController.createCityWeather);
+router.put("/api/city", jwtAuth, invalidateCache('/api/city'), CityWeatherController.editCityWeather);
+router.delete("/api/city", jwtAuth, invalidateCache('/api/city'), CityWeatherController.deleteCityWeather);
+
+// Obtem climas junto com api.weatherstack
 router.get("/api/weather", jwtAuth, CityWeatherController.getWeather);
 
 module.exports = router;
