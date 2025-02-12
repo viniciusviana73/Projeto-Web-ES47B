7a242817fd499917e0cdc6121c3b8f9d857efd1a
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2025-02-11 23:57:55 -0300

    Restful API 'cityweather'

--- ES47B-P2-Backend/src/router/router.js
+++ ES47B-P2-Backend/src/router/router.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
 const UserController = require("../controllers/UserController");
 const { jwtAuth } = require("../middlewares/jwtAuth");
-
-    console.log(req.body);
-    return res.json(req.body);
-});
+const CityWeatherController = require("../controllers/CityWeatherController");
 
 // Auth API
 router.get("/api/auth", jwtAuth, UserController.checkAuth);
@@ -13,5 +10,12 @@ router.post("/api/auth", UserController.login);
 // User 
 router.get("/api/user", UserController.getUser);
 router.post("/api/user", UserController.createUser);
+
+// CityWeather API
+router.get("/api/cityweather", CityWeatherController.getCityWeather);
+router.post("/api/cityweather", CityWeatherController.createCityWeather);
+router.put("/api/cityweather", CityWeatherController.editCityWeather);
+router.delete("/api/cityweather", CityWeatherController.deleteCityWeather);
+router.get("/api/weather", CityWeatherController.getWeather); // Recupera clima de todos CityWeather (para renderizarmos todas cidades cadastradas no front)
 
 module.exports = router;
