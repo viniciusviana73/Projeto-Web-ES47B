3f40d08b534abe739aa2f81444d3660b94e1cd33
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2025-02-12 16:03:41 -0300

    mongoose - maxPoolSize

--- ES47B-P2-Backend/src/database/connection.js
+++ ES47B-P2-Backend/src/database/connection.js
@@ -4,15 +4,17 @@ mongoose.set('strictQuery', false);
 
 const connect = async () => {
     const maxRetries = process.env.MAX_DB_CONN_RETRIES || 5;
+    const maxPoolSize = process.env.DB_POOL_SIZE || 10;
     let retryCount = 0;
 
     while (retryCount < maxRetries) {
         try {
-            const conn = await mongoose.connect(process.env.MONGO_URI, {
-                //useNewUrlParser: true,
-                dbName: process.env.DB_NAME
+            await mongoose.connect(process.env.MONGO_URI, {
+                dbName: process.env.DB_NAME,
+                maxPoolSize: parseInt(maxPoolSize, 10),
             });
-            console.log('MongoDB conectado com sucesso')
+
+            console.log('MongoDB conectado com sucesso');
             return;
         } catch (error) {
             console.log(`Tentativa de conexão ${retryCount + 1} falhou: ${error}`);
@@ -22,6 +24,6 @@ const connect = async () => {
 
     console.log(`Número máximo de tentativas (${maxRetries}) excedido. Fechando servidor.`);
     process.exit(1);
-}
+};
 
 module.exports = connect;
