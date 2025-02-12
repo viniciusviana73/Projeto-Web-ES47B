d8e2da3caf325073ae6f8a344235ddbb8b9fbd63
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2025-02-12 01:01:15 -0300

    middleware jwtAuth em rotas protegidas por autenticação

--- ES47B-P2-Backend/src/router/router.js
+++ ES47B-P2-Backend/src/router/router.js
@@ -8,14 +8,14 @@ router.get("/api/auth", jwtAuth, UserController.checkAuth);
 router.post("/api/auth", UserController.login);
 
 // User 
-router.get("/api/user", UserController.getUser);
+router.get("/api/user", jwtAuth, UserController.getUser);
 router.post("/api/user", UserController.createUser);
 
 // CityWeather API
-router.get("/api/cityweather", CityWeatherController.getCityWeather);
-router.post("/api/cityweather", CityWeatherController.createCityWeather);
-router.put("/api/cityweather", CityWeatherController.editCityWeather);
-router.delete("/api/cityweather", CityWeatherController.deleteCityWeather);
-router.get("/api/weather", CityWeatherController.getWeather); // Recupera clima de todos CityWeather (para renderizarmos todas cidades cadastradas no front)
+router.get("/api/cityweather", jwtAuth, CityWeatherController.getCityWeather);
+router.post("/api/cityweather", jwtAuth, CityWeatherController.createCityWeather);
+router.put("/api/cityweather", jwtAuth, CityWeatherController.editCityWeather);
+router.delete("/api/cityweather", jwtAuth, CityWeatherController.deleteCityWeather);
+router.get("/api/weather", jwtAuth, CityWeatherController.getWeather);
 
 module.exports = router;
